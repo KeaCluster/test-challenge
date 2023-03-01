748f176440d7b7a2a36b7125f4d9aabce5980729
Zabdiel Diaz <37281456+KeaCluster@users.noreply.github.com>
2023-03-01 15:36:59 -0600

    Updated methods
    Removed expected answers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,22 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    expect(calculator.sum(3,4)).toBe(7);
+    //
 })
 
 test('substracts a - b', () => {
-    expect(calculator.substract(5,3)).toBe(2);
+   //
 })
 
 test('multiplies a * b', () => {
-    expect(calculator.multiply(10,3)).toBe(30);
+    //
 })
 
 test('divides a / b', () => {
-    expect(calculator.divide(6,2)).toBe(3);
+    // Consider decimals
 })
 
 test('Raises to power a ^ b', () => {
-    expect(calculator.power(2,4)).toBe(16);
+    //
 })
 
