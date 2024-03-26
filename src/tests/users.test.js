e9f162b0994a33457c1e99f0cee1adf58482b5f4
KeaCluster <zzabdiel29@gmail.com>
2024-03-25 22:42:26 -0600

    Cleanup files

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,9 +1,9 @@
 // You already have the methods imported
-const { default: expect } = require('expect');
-const {getUser, getUsers} = require('../users');
-
+import { default as expect } from "expect";
+import { getUser, getUsers } from "../users";
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
 
 // READ documentation
+
--- src/users.js
+++ src/users.js
@@ -1,19 +1,19 @@
 // Imports small array of users
 // Pretend it's an API request
-const users = require('./data');
+const users = require("./data").default;
 
 // Fetches all users
 const getUsers = () => {
-    return users;
-}
+  return users;
+};
 
 // Filters users by specific ID
-const getUser = id => {
-    return users.find(user => user.id === id)
-}
-
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
 
 // test
 // console.log(getUser(3));
 
-module.exports = {getUsers , getUser};
+module.exports = { getUsers, getUser };
+
